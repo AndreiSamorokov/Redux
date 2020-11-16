421cc19c1c1e3c31dc718e434d436cea7f1e98bf
AndreiSamorokov <andreiv0202@gmail.com>
2020-11-17 05:10:46 +0900

    react-redux-counter

--- redux-react-counter/src/App.js
+++ redux-react-counter/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { createStore } from 'redux' 
+import Counter from './components/Counter'
+import counter from './reducers'
+
+const store = createStore(counter)
 
 function App() {
+  const [ storeitem, setStoreItem ] = React.useState(0);
+   
+  store.subscribe(() => {
+    setStoreItem( store.getState() )
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+        <Counter
+          value={storeitem}
+          onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+          onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        /> 
     </div>
   );
 }
