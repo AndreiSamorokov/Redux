3778ad0afd4f2166cb6848ec837f2eab7c167fb8
AndreiSamorokov <andreiv0202@gmail.com>
2020-11-23 06:07:49 +0900

    adding add pro feature

--- shopping_cart/src/componnents/Product.js
+++ shopping_cart/src/componnents/Product.js
@@ -1,8 +1,13 @@
 function Product({title, price, inventory}) {
+
+    const addProduct = (e) =>{
+      console.log( 'add product ' + e); 
+    }
+
     return (
       <div> 
           <div> {title} - {price} - {inventory} </div> 
-          <button > Add to cart </button>
+          <button onClick={addProduct}> Add to cart </button>
       </div>
     );
   }
